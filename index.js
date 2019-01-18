e5fb1b919b5bbf3211e10dc780a7d23d532da48f
Stefan Wojcik <ert.mcscrad@gmail.com>
2019-01-17 21:33:15 -0500

    switched to coingecko api

--- index.js
+++ index.js
@@ -4,36 +4,66 @@ const app = express()
 const request = require('request')
 const PORT = process.env.PORT || 5000
 
-let cache = {};
-let lastUpdateTime;
+const cache = {}
+const coinSymbolToIdMapping = {}
 
-function updateCache(cb) {
+function cacheCoinList(cb) {
   request({
     json: true,
-    qs: {
-      convert: 'CAD',
-      limit: 0,
-    },
     method: 'GET',
     timeout: 1000 * 30,
-    url: 'https://api.coinmarketcap.com/v1/ticker',
+    url: `https://api.coingecko.com/api/v3/coins/list`,
   }, (err, resp, body) => {
     if (err || resp.statusCode >= 400) {
-      console.error('Failed to get cmc update.', err, body, new Date())
-      return res.status(500).json(err || body)
+      console.error('Failed to get coingecko coin info.', err, body, new Date())
+      return cb(err || body)
     }
-    body.forEach(info => cache[info.symbol.toUpperCase()] = info)
-    lastUpdateTime = new Date()
-    // console.log('prices updated', lastUpdateTime)
-    cb && cb()
+    body.forEach((coin) => {
+      coinSymbolToIdMapping[coin.symbol] = coin.id
+    })
+    console.log('successfully cached coin list')
+    cb()
   })
 }
 
-updateCache(() => setInterval(updateCache, 1000 * 60))
+function cacheCoinInfo(symbol, cb) {
+  if (!coinSymbolToIdMapping[symbol]) return cb('404')
+  request({
+    json: true,
+    method: 'GET',
+    timeout: 1000 * 30,
+    url: `https://api.coingecko.com/api/v3/coins/${coinSymbolToIdMapping[symbol]}`,
+  }, (err, resp, body) => {
+    if (err || resp.statusCode >= 400) {
+      console.error('Failed to get coingecko coin info.', err, body, new Date())
+      return cb(err || body)
+    }
+    cache[symbol] = { price: body.market_data.current_price.usd, lastUpdateTime: new Date() }
+    console.log(`successfully cached symbol: ${symbol}, price: ${body.market_data.current_price.usd}`)
+    cb()
+  })
+}
 
 app.get('/:symbol', (req, res) => {
-  const price = _.get(cache, [req.params.symbol.toUpperCase(), 'price_cad'], 0)
-  res.json(Number(price))
+  const symbol = req.params.symbol.toLowerCase()
+  function finish() {
+    res.json(cache[symbol].price)
+  }
+  if (cache[symbol] && new Date() - cache[symbol].lastUpdateTime < 1000 * 60 * 10) {
+    return finish()
+  } else if (_.isEmpty(cache)) {
+    return cacheCoinList((err) => {
+      if (err) return res.status(500).json(err)
+      cacheCoinInfo(symbol, (err) => {
+        if (err) return res.status(500).json(err)
+        finish()
+      })
+    })
+  }
+  cacheCoinInfo(symbol, (err) => {
+    if (err) return res.status(500).json(err)
+    finish()
+  })
 })
 
-app.listen(PORT, () => console.log('Example app started!'))
+app.listen(PORT, () => console.log(`Example app started on port: ${PORT}!`))
